8cc1ec4b92344fbd8482e546834fa4ca62ad1a97
Generoso Pagano <generoso.pagano@inria.fr>
2015-05-12 16:08:52 +0200

    license

--- fr.inria.soctrace.tools.importer.kptracecsvcat/src/fr/inria/soctrace/tools/importer/kptcsv/core/KptcsvTraceManager.java
+++ fr.inria.soctrace.tools.importer.kptracecsvcat/src/fr/inria/soctrace/tools/importer/kptcsv/core/KptcsvTraceManager.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Inria.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * 	Alexis Martin - initial API and implementation
+ ******************************************************************************/
 package fr.inria.soctrace.tools.importer.kptcsv.core;
 
 import fr.inria.soctrace.lib.model.Trace;
